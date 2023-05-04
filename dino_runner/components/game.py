e21d3cd2084585a4eb8cd952825624288c5852fd
Elias Junior <santanaeliasjunior@gmail.com>
2023-05-04 00:02:53 -0300

    Finalizando o Jogo Base

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,5 @@
 import pygame
 
-
-
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
 
 from dino_runner.components.dinosaur import Dinosaur
@@ -19,11 +16,11 @@ from dino_runner.components.powerups.power_up_manager import PowerUpManager
 
 class Game:
 
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
         self.running = False
@@ -41,6 +38,7 @@ class Game:
         while self.running:
             if not self.playing:
                 self.show_menu()
+
         pygame.display.quit()
         pygame.quit()
 
@@ -70,28 +68,29 @@ class Game:
 
     def update_score(self):
         self.score += 1
-        if self.score %100 == 0:
+        if self.score % 100 == 0:
             self.game_speed +=5
     
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255,255,255))
         self.draw_background()
+        self.draw_score()
+        self.draw_power_up_time()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
-        self.draw_power_up_time()
         self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
-        image_width = BG.get_width
+        image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG,(image_width + self.x_pos_bg, self.y_pos_bg))
-        if self.x_pos_bg <= image_width:
-            self.screen.blit(BG, image_width + self.x_pos_bg, self.y_pos_bg)
+        if self.x_pos_bg <= -image_width:
+            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
-            self.x_pos_bg -= self.game_speed
+        self.x_pos_bg -= self.game_speed
     
     def draw_score(self):
         draw_message_component (
@@ -103,7 +102,7 @@ class Game:
 
     def draw_power_up_time(self):
         if self.player.has_power_up:
-            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks() / 1000,2))
+            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks()) / 1000, 2)
             if time_to_show >= 0:
                 draw_message_component(
                     f"{self.player.type.capitalize()} enable for {time_to_show} seconds",
@@ -127,7 +126,8 @@ class Game:
     def show_menu(self):
         self.screen.fill((255,255,255))
         half_screen_height = SCREEN_HEIGHT // 2
-        half_screen_width = SCREEN_WIDTH
+        half_screen_width = SCREEN_WIDTH // 2
+
         if self.death_count == 0:
             draw_message_component(
                 "Pressione qualquer tecla para iniciar o jogo",
@@ -151,4 +151,6 @@ class Game:
             self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 30))
         pygame.display.flip()
         self.handle_events_on_menu()
+
+
         
